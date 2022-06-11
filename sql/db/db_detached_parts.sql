5d3f9e02543ccfd05e6eb6b4a9df476a7a9f189c
O2eg <oleg.ispu@yandex.ru>
2022-06-12 00:40:44 +0300

    sql formatting

--- sql/db/db_detached_parts.sql
+++ sql/db/db_detached_parts.sql
@@ -1,10 +1,10 @@
 select
 	_shard_num,
 	hostName() as host_name,
-    database,
-    table,
-    count(1) as count_detached_parts,
-    arrayDistinct(groupArray(reason)) as reasons
+	database,
+	table,
+	count(1) as count_detached_parts,
+	arrayDistinct(groupArray(reason)) as reasons
 from clusterAllReplicas(_CLUSTER_NAME, system.detached_parts)
 group by _shard_num, host_name, database, table
 order by count_detached_parts desc
