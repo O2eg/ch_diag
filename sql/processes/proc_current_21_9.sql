63efef110c75d9a7b8d44e450457108dd2725502
O2eg <oleg.ispu@yandex.ru>
2022-07-03 23:27:17 +0300

    formatting

--- sql/processes/proc_current_21_9.sql
+++ sql/processes/proc_current_21_9.sql
@@ -1,19 +1,19 @@
 SELECT
 	_shard_num,
 	hostName() as host_name,
-    query,
-    query_id,
-    is_initial_query,
-    initial_query_id,
-    round(elapsed, 1) as _elapsed,
+	query,
+	query_id,
+	is_initial_query,
+	initial_query_id,
+	round(elapsed, 1) as _elapsed,
 	read_rows,
 	read_bytes,
 	written_rows,
 	written_bytes,
-    memory_usage,
-    peak_memory_usage,
-    formatReadableSize(memory_usage) as pretty_memory_usage,
-    formatReadableSize(peak_memory_usage) as pretty_peak_memory_usage
+	memory_usage,
+	peak_memory_usage,
+	formatReadableSize(memory_usage) as pretty_memory_usage,
+	formatReadableSize(peak_memory_usage) as pretty_peak_memory_usage
 from clusterAllReplicas(_CLUSTER_NAME, system.processes)
 order by _elapsed desc
 limit 1000
